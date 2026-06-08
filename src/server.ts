96ecd6302d1150f491027e3021a9517ea5653bf0
ajith736 <ajithc736@gmail.com>
2026-06-08 19:19:07 +0530

    Add User model to Prisma schema; update server to connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import {app} from './app.js'; 
 import {PORT} from './config/env.js';
+import { connectDatabase } from './config/database.js';
 
-export function startserver(){
+ async function startserver(){
+    await connectDatabase();
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
     });
