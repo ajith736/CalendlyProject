44932444b146e75ac0d179a8d45832b834c34584
ajith736 <ajithc736@gmail.com>
2026-06-23 13:23:53 +0530

    added more graceful error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { userRouter } from './routers/user.router.js';
+import { errorHandler } from './middlewares/error-handler.js';
 
 
 const app = express();
+app.use(express.json()); // to parse the body of the request (deserialize the body of the request)
 
 app.get('/health', (_req, res) => {
     res.json({
@@ -12,5 +14,6 @@ app.get('/health', (_req, res) => {
 });
 
 app.use("/api/users",userRouter); // if the route starts with /users , the userRouter will handle it
+app.use(errorHandler);
 
-export {app};
+export {app};  
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import { findAllUsers, findById } from '../controllers/user.controller.js';
+import { findAllUsers, findById, createUser } from '../controllers/user.controller.js';
 
 
 export const userRouter : Router = Router();
 userRouter.get('/', findAllUsers);
 userRouter.get('/:id', findById);
+userRouter.post('/', createUser);
 
